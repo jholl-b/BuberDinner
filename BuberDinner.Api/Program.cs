1f8f91cb899650e44c1a665ca9ee0b8255042276
Jonathan Barreira <jonathanbarreira@gmail.com>
2022-08-25 21:41:31 -0300

    video 9

--- BuberDinner.Api/Program.cs
+++ BuberDinner.Api/Program.cs
@@ -21,6 +21,7 @@ var app = builder.Build();
     //app.UseMiddleware<ErrorHandlingMiddleware>();
     app.UseExceptionHandler("/error");
     app.UseHttpsRedirection();
+    app.UseAuthentication();
     app.UseAuthorization();
     app.MapControllers();
     app.Run();
